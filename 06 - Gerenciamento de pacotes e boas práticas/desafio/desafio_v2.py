f96301c08f1305dda633b6df4eae2712a2f58315
oacir-br <oacir.c@gmail.com>
2025-12-16 12:45:08 -0400

    Implementa conceitos de POO

--- 06 - Gerenciamento de pacotes e boas práticas/desafio/desafio_v2.py
+++ 06 - Gerenciamento de pacotes e boas práticas/desafio/desafio_v2.py
@@ -1,5 +1,5 @@
 import textwrap
-from abc import ABC, abstractclassmethod, abstractproperty
+from abc import ABC, abstractmethod
 from datetime import datetime
 
 
@@ -182,11 +182,12 @@ class Historico:
 
 class Transacao(ABC):
     @property
-    @abstractproperty
+    @abstractmethod
     def valor(self):
         pass
 
-    @abstractclassmethod
+    @classmethod
+    @abstractmethod
     def registrar(self, conta):
         pass
 
